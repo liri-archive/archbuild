276e3d4a58e2ae60d8785cc9a539d5b33b58bd8a
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-10-12 08:23:01 +0200

    livecd: Create unprivileged user for the live session

--- livecd/airootfs/root/customize_airootfs.sh
+++ livecd/airootfs/root/customize_airootfs.sh
@@ -11,6 +11,9 @@ usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
+useradd -c 'Live User' -m -G wheel -U liveuser
+passwd -d liveuser
+
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
