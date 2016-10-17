855af9297f2dcb7ee4057f551976c5c762a78e32
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-10-17 22:18:39 +0200

    livecd: Remove dual suffix from ISO file names
    Our ISO is x86_64 only.

--- livecd/build.sh
+++ livecd/build.sh
@@ -215,7 +215,7 @@ make_prepare() {
 
 # Build ISO
 make_iso() {
-    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-dual.iso"
+    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}.iso"
 }
 
 if [[ ${EUID} -ne 0 ]]; then
